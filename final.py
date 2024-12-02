98abeb1c1b3fa1a7b3e7ad7b0a9d66c5b0c688af
zatchet <zach@rippas.org>
2024-12-01 21:20:38 -0500

    removed uni name from question

--- final.py
+++ final.py
@@ -1,6 +1,12 @@
 from secret_retriever import retrieve_secret
 import praw
 import prawcore
+from openai import OpenAI
+
+# initializing openai configuration
+BASE_URL = retrieve_secret('cs4973_base_url')
+API_KEY=api_key = retrieve_secret('cs4973_api_key')
+client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
 
 from subreddit import get_subreddit_name
 from fact_opinion_classification.classifier import predict
@@ -10,6 +16,27 @@ api_client = praw.Reddit(
     client_secret=retrieve_secret('praw_client_secret'),
     user_agent=retrieve_secret('praw_user_agent')
 )
+
+def remove_uni_name(question):
+    SYSTEM_PROMPT = """
+    Given the question, return the name of the university or college that is being asked about.
+    Return exactly as it appears in the question.
+
+    Question: How do people feel about the hatred against Palestine at BU?
+    Answer: BU
+    Question: At Harvard University, what is the overall opinion of the party scene?
+    Answer: Harvard University
+    """
+
+    messages = [{'role': 'system', 'content': SYSTEM_PROMPT}]
+    messages.extend([{'role': 'user', 'content': question}])
+    resp = client.chat.completions.create(
+        messages = messages,
+        model = "meta-llama/Meta-Llama-3.1-8B-Instruct",
+        temperature=0.7)
+
+    model_response = resp.choices[0].message.content
+    return question.replace(model_response, '').strip()
 
 def get_subreddit(question):
     subreddit_name = get_subreddit_name(question)
@@ -29,8 +56,8 @@ def process_query(query: str):
     # remove stop words and other words that are not useful
     with open('stopwords.txt', 'r') as f:
         stopwords = f.read().split('\n')
-    words_to_remove = stopwords + ['university', 'college']
-    return ' '.join([word for word in query.split() if word not in words_to_remove])
+    # print(query.split())
+    return ' '.join([word for word in remove_uni_name(query).split() if word.lower() not in stopwords])
 
 def get_top_post(query: str):
     subreddit = get_subreddit(query)
